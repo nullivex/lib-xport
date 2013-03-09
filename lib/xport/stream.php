ead3c62fd7c7c7d734ebeccf904d5fd2ea5b7f93
Nullivex <contact@nullivex.com>
2013-03-08 23:02:26 -0800

    updated xport stream bandler

--- lib/xport/stream.php
+++ lib/xport/stream.php
@@ -3,10 +3,6 @@ lib('crypt');
 
 class XportStream {
 
-	//const
-	const HDR_ENCRYPT_NAME = 'OpenLSS-Crypt';
-	const HDR_COMPRESS_NAME = 'compressed';
-
 	//resources
 	protected $crypt = null;
 
@@ -14,18 +10,30 @@ class XportStream {
 	protected $encrypt = false;
 	protected $compress = false;
 
-	protected $func_compress = 'gzdeflate';
-	protected $func_decompress = 'gzinflate';
-
 	protected $payload = null;
 	protected $size = null;
 
-	public function _get($payload){
-		return new self($payload,Config::get('crypt','key'),Config::get('crypt','iv'));
+	//compression algs
+	const COMPRESS_OFF = 0x00;
+	const COMPRESS_GZIP = 0x01;
+	const COMPRESS_BZIP = 0x02;
+	const COMPRESS_LZF = 0x03;
+
+	//crypt handlers
+	const CRYPT_OFF = 0x00;
+	const CRYPT_LSS = 0x01;
+
+	public static function receive($payload){
+		$stream = self::_get();
+		$stream->setPayload($payload);
+		return $stream->decode();
 	}
 
-	public function __construct($payload,$crypt_key,$crypt_iv){
-		$this->setPayload($payload);
+	public static function _get(){
+		return new self(Config::get('crypt','key'),Config::get('crypt','iv'));
+	}
+
+	public function __construct($crypt_key,$crypt_iv){
 		$this->crypt = Crypt::_get($crypt_key,$crypt_iv);
 	}
 
@@ -37,138 +45,115 @@ class XportStream {
 	//-----------------------------------------------------
 	//Stream Modifiers
 	//-----------------------------------------------------
-	public function setByHeaders($headers){
-		if(!is_array($headers))
-			throw new Exception('Headers are not array for tstream setup');
-		if(mda_get($headers,'X-Content-Encryption') == self::HDR_ENCRYPT_NAME)
-			$this->encrypt = true;
-		else
-			$this->encrypt = false;
-		if(mda_get($headers,'X-Content-Encoding') == self::HDR_COMPRESS_NAME)
-			$this->compress = true;
-		else
-			$this->compress = false;
-		return $this;
-	}
-
-	public function setByParams($params){
-		if(!is_array($params))
-			throw new Exception('Params are not array for tstream setup');
-		if(is_numeric(mda_get($params,'compression')))
-			$this->compress = true;
-		else
-			$this->compress = false;
-		if(mda_get($params,'encryption') == 'true')
-			$this->encrypt = true;
-		else
-			$this->encrypt = false;
-		return $this;
-	}
-
 	public function setEncryption($flag=false){
 		$this->encrypt = $flag;
 		return $this;
 	}
 
-	public function setCompression($level=0){
-		if(!is_numeric($level) || $level < 0 || $level > 9)
-			throw new Exception('Invalid compression setting, must be integer 0-9: '.$level);
-		if($level == 0)
-			$this->compress = false;
-		else
-			$this->compress = $level;
+	public function setCompression($flag=false){
+		$this->compress = false;
 		return $this;
 	}
 
-	public function setCompressCallback($callback){
-		if(!is_callable($callback))
-			throw new Exception('Invalid compress callback passed: '.$callback);
-		$this->func_compress = $callback;
-		return $this;
+	//-----------------------------------------------------
+	//Process Functions
+	//-----------------------------------------------------
+	protected function compress(){
+		if($this->compress === self::COMPRESS_OFF) return true;
+		switch($this->compress){
+			case self::COMPRESS_GZIP:
+				$this->payload = gzdeflate($this->payload);
+				break;
+			case self::COMPRESS_BZIP:
+				$this->payload = bzcompress($this->payload);
+				break;
+			case self::COMPRESS_LZF:
+				$this->payload = lzf_compress($this->payload);
+				break;
+			default:
+				return true;
+		}
 	}
 
-	public function setDecompressCallback($callback){
-		if(!is_callable($callback))
-			throw new Exception('Invalid decompress callback passed: '.$callback);
-		$this->func_decompress = $callback;
-		return $this;
+	protected function decompress(){
+		if($this->compress === self::COMPRESS_OFF) return true;
+		switch($this->compress){
+			case self::COMPRESS_GZIP:
+				$this->payload = gzinflate($this->payload);
+				break;
+			case self::COMPRESS_BZIP:
+				$this->payload = bzdecompress($this->payload);
+				break;
+			case self::COMPRESS_LZF:
+				$this->payload = lzf_decompress($this->payload);
+				break;
+			default:
+				//void
+				break;
+		}
+		return true;
 	}
 
-	//--------------------------------------------------------
-	//Internal Handlers
-	//--------------------------------------------------------
-	protected function chksumCreate(){
-		$this->md5 = md5($this->payload);
-		$this->sha1 = sha1($this->payload);
+	protected function encrypt(){
+		if($this->encrypt === self::CRYPT_OFF) return true;
+		switch($this->encrypt){
+			case self::CRYPT_LSS:
+				$size = strpad($this->size,10,0,STR_PAD_LEFT);
+				$this->payload = $this->crypt->encrypt($size.$this->payload);
+				break;
+			default:
+				//void
+				break;
+		}
+		return true;
 	}
 
-	protected function chksumVerify(){
-		$md5 = md5($this->payload);
-		$sha1 = sha1($this->payload);
-		if($sha1 !== $this->sha1)
-			throw new Exception('Payload hash check (sha1) failed');
-		if($md5 !== $this->md5)
-			throw new Exception('Payload hash check (md5) failed');
+	protected function decrypt(){
+		if($this->encrypt === self::CRYPT_OFF) return true;
+		switch($this->encrypt){
+			case self::CRYPT_LSS:
+				$this->payload = $this->crypt->decrypt($this->payload);
+				$size = substr($this->payload,0,10);
+				$this->payload = substr($this->payload,10,$size);
+				break;
+			default:
+				//void
+				break;
+		}
+		return true;
 	}
 
-	protected function pad(){
-		$block_size = $this->crypt->getBlockSize();
-		$this->payload = str_pad($this->payload,$block_size,"\0",STR_PAD_RIGHT);
+	protected function setup(){
+		$set = ord(substr($this->payload,0,1));
+		$this->payload = substr($this->payload,1);
+		$this->encrypt = $set >> 4;
+		$this->compress = $set & 0xf0;
 	}
 
-	protected function unpad(){
-		$this->payload = substr($this->payload,0,$this->size);
+	protected function finalize(){
+		$set = chr((($this->encrypt << 4) | $this->compress) & 0xff));
+		$this->payload = chr($set).$this->payload;
 	}
 
 	//-----------------------------------------------------
 	//Process Functions
 	//-----------------------------------------------------
 	public function encode(){
-		$this->chksumCreate();
-		if($this->encrypt !== false){
-			$this->pad();
-			$this->payload = $this->crypt->encrypt($this->payload);
-		}
-		if($this->compress !== false)
-			$this->payload = call_user_func_array($this->func_compress,$this->payload);
-		return $this;
+		$this->encrypt();
+		$this->compress();
+		$this->finalize();
+		return $this->payload;
 	}
 
 	public function decode(){
-		if($this->compress !== false)
-			$this->payload = call_user_func_array($this->func_decompress,$this->payload);
-		if($this->encrypt !== false){
-			$this->payload = $this->crypt->decrypt($this->payload);
-			$this->unpad();
-		}
-		$this->chksumVerify();
-		return $this;
+		$this->setup();
+		$this->decompress();
+		$this->decrypt();
+		return $this->payload();
 	}
 
 	public function get(){
 		return $this->payload;
-	}
-
-	public function headers(){
-		$headers = array();
-		if($this->compress !== false)
-			$headers[] = 'X-Content-Encoding: '.self::HDR_COMPRESS_NAME;
-		if($this->encrypt)
-			$headers[] = 'X-Content-Encryption: '.self::HDR_ENCRYPT_NAME;
-		return $headers;
-	}
-
-	public function params(){
-		$params = array();
-		if($this->compress !== false)
-			$params['compression'] = $this->compress;
-		else
-			$params['compression'] = 'false';
-		if($this->encrypt !== false)
-			$params['encryption'] = 'true';
-		else
-			$params['encryption'] = 'false';
-		return $params;
 	}
 
 }
